5274308e2e3585237bb5f06acfc21f11d2bb6519
waronalolosamuel06 <waronalolosamuel@gmail.com>
2026-04-20 17:54:16 +0200

    Refactor Vaccine class constructor and methods

--- src/com/ub/csimedicinetracker/model/Vaccine.java
+++ src/com/ub/csimedicinetracker/model/Vaccine.java
@@ -1,25 +1,23 @@
 package com.ub.csimedicinetracker.model;
 
 public class Vaccine extends Medicine {
-        private String manufacturer;
-            private double storageTemp;
+private String manufacturer;
+private double storageTemp;
 
-                // Constructor linking to the parent (Medicine) fields
-                    public Vaccine(String name, String batchID, String expiryDate, Stock inventory, String manufacturer, double storageTemp) {
-                                super(name, batchID, expiryDate, inventory);
-                                        this.manufacturer = manufacturer;
-                                                this.storageTemp = storageTemp;
+ // Constructor linking to the parent (Medicine) fields
+public Vaccine(String name, String batchID, String expiryDate, Stock inventory, String manufacturer, double storageTemp) {
+super(name, batchID, expiryDate, inventory);
+this.manufacturer = manufacturer;
+ this.storageTemp = storageTemp;
                     }
 
-                        @Override
-                            public void displayDetails() {
-                                        System.out.println("--- Vaccine Details ---");
-                                                System.out.println("Name: " + getName());
-                                                        System.out.println("Manufacturer: " + manufacturer);
-                                                                System.out.println("Storage Temp: " + storageTemp + "°C");
-                                                                        System.out.println("Current Stock: " + getInventory().getQuantity());
+@Override
+public void displayDetails() {
+System.out.println("--- Vaccine Details ---");
+System.out.println("Name: " + getName());
+System.out.println("Manufacturer: " + manufacturer);
+System.out.println("Storage Temp: " + storageTemp + "°C");
+System.out.println("Current Stock: " + getInventory().getQuantity());
                             }
-
-
-                            
-                    }
+      
+ }
